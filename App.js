cba3c9c0782a06dae18b18b62eaaac73322e786f
Viet Nguyen <viet.nguyen@successsoftware.global>
2022-03-29 09:20:35 +0700

    second commit

--- App.js
+++ App.js
@@ -1,112 +1,322 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+// import {StatusBar} from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  ImageBackground,
+  Pressable,
+  Alert,
+  StatusBar,
 } from 'react-native';
+import bg from './src/assets/images/bg.jpeg';
+import Cell from './src/components/Cell';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const emptyMap = [
+  ['', '', ''], // 1st row
+  ['', '', ''], // 2nd row
+  ['', '', ''], // 3rd row
+];
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const copyArray = original => {
+  console.log('ghe');
+  console.log(original);
+  const copy = original.map(arr => {
+    return arr.slice();
+  });
+  console.log(copy);
+  return copy;
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App() {
+  const [map, setMap] = useState(emptyMap);
+  const [currentTurn, setCurrentTurn] = useState('x');
+  const [gameMode, setGameMode] = useState('BOT_MEDIUM'); // LOCAL, BOT_EASY, BOT_MEDIUM;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  useEffect(() => {
+    if (currentTurn === 'o' && gameMode !== 'LOCAL') {
+      botTurn();
+    }
+  }, [currentTurn, gameMode]);
+
+  useEffect(() => {
+    const winner = getWinner(map);
+    if (winner) {
+      gameWon(winner);
+    } else {
+      checkTieState();
+    }
+  }, [map]);
+
+  const onPress = (rowIndex, columnIndex) => {
+    if (map[rowIndex][columnIndex] !== '') {
+      Alert.alert('Position already occupied');
+      return;
+    }
+
+    setMap(existingMap => {
+      const updatedMap = [...existingMap];
+      updatedMap[rowIndex][columnIndex] = currentTurn;
+      return updatedMap;
+    });
+
+    setCurrentTurn(currentTurn === 'x' ? 'o' : 'x');
+  };
+
+  const getWinner = winnerMap => {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      const isRowXWinning = winnerMap[i].every(cell => cell === 'x');
+      const isRowOWinning = winnerMap[i].every(cell => cell === 'o');
+
+      if (isRowXWinning) {
+        return 'x';
+      }
+      if (isRowOWinning) {
+        return 'o';
+      }
+    }
+
+    // Check columns
+    for (let col = 0; col < 3; col++) {
+      let isColumnXWinner = true;
+      let isColumnOWinner = true;
+
+      for (let row = 0; row < 3; row++) {
+        if (winnerMap[row][col] !== 'x') {
+          isColumnXWinner = false;
+        }
+        if (winnerMap[row][col] !== 'o') {
+          isColumnOWinner = false;
+        }
+      }
+
+      if (isColumnXWinner) {
+        return 'x';
+      }
+      if (isColumnOWinner) {
+        return 'o';
+      }
+    }
+
+    // check diagonals
+    let isDiagonal1OWinning = true;
+    let isDiagonal1XWinning = true;
+    let isDiagonal2OWinning = true;
+    let isDiagonal2XWinning = true;
+    for (let i = 0; i < 3; i++) {
+      if (winnerMap[i][i] !== 'o') {
+        isDiagonal1OWinning = false;
+      }
+      if (winnerMap[i][i] !== 'x') {
+        isDiagonal1XWinning = false;
+      }
+
+      if (winnerMap[i][2 - i] !== 'o') {
+        isDiagonal2OWinning = false;
+      }
+      if (winnerMap[i][2 - i] !== 'x') {
+        isDiagonal2XWinning = false;
+      }
+    }
+
+    if (isDiagonal1OWinning || isDiagonal2OWinning) {
+      return 'o';
+    }
+    if (isDiagonal1XWinning || isDiagonal2XWinning) {
+      return 'x';
+    }
+  };
+
+  const checkTieState = () => {
+    if (!map.some(row => row.some(cell => cell === ''))) {
+      Alert.alert("It's a tie", 'tie', [
+        {
+          text: 'Restart',
+          onPress: resetGame,
+        },
+      ]);
+    }
+  };
+
+  const gameWon = player => {
+    Alert.alert('Huraaay', `Player ${player} won`, [
+      {
+        text: 'Restart',
+        onPress: resetGame,
+      },
+    ]);
+  };
+
+  const resetGame = () => {
+    setMap([
+      ['', '', ''], // 1st row
+      ['', '', ''], // 2nd row
+      ['', '', ''], // 3rd row
+    ]);
+    setCurrentTurn('x');
+  };
+
+  const botTurn = () => {
+    // collect all possible options
+    const possiblePositions = [];
+    map.forEach((row, rowIndex) => {
+      row.forEach((cell, columnIndex) => {
+        if (cell === '') {
+          possiblePositions.push({row: rowIndex, col: columnIndex});
+        }
+      });
+    });
+
+    let chosenOption;
+
+    if (gameMode === 'BOT_MEDIUM') {
+      // Attack
+      possiblePositions.forEach(possiblePosition => {
+        const mapCopy = copyArray(map);
+
+        mapCopy[possiblePosition.row][possiblePosition.col] = 'o';
+
+        const winner = getWinner(mapCopy);
+        if (winner === 'o') {
+          // Attack that position
+          chosenOption = possiblePosition;
+        }
+      });
+
+      if (!chosenOption) {
+        // Defend
+        // Check if the opponent WINS if it takes one of the possible Positions
+        possiblePositions.forEach(possiblePosition => {
+          const mapCopy = copyArray(map);
+
+          mapCopy[possiblePosition.row][possiblePosition.col] = 'x';
+
+          const winner = getWinner(mapCopy);
+          if (winner === 'x') {
+            // Defend that position
+            chosenOption = possiblePosition;
+          }
+        });
+      }
+    }
+
+    // choose random
+    if (!chosenOption) {
+      chosenOption =
+        possiblePositions[Math.floor(Math.random() * possiblePositions.length)];
+    }
+
+    if (chosenOption) {
+      onPress(chosenOption.row, chosenOption.col);
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
+    <View style={styles.container}>
+      <ImageBackground source={bg} style={styles.bg} resizeMode="contain">
+        <Text
           style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+            fontSize: 24,
+            color: 'white',
+            position: 'absolute',
+            top: 50,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          Current Turn: {currentTurn.toUpperCase()}
+        </Text>
+        <View style={styles.map}>
+          {map.map((row, rowIndex) => (
+            <View key={`row-${rowIndex}`} style={styles.row}>
+              {row.map((cell, columnIndex) => (
+                <Cell
+                  key={`row-${rowIndex}-col-${columnIndex}`}
+                  cell={cell}
+                  onPress={() => onPress(rowIndex, columnIndex)}
+                />
+              ))}
+            </View>
+          ))}
         </View>
-      </ScrollView>
-    </SafeAreaView>
+
+        <View style={styles.buttons}>
+          <Pressable onPress={() => setGameMode('LOCAL')}>
+            <Text
+              style={[
+                styles.button,
+                {backgroundColor: gameMode === 'LOCAL' ? '#4F5686' : '#191F24'},
+              ]}>
+              Local
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={() => setGameMode('BOT_EASY')}>
+            <Text
+              style={[
+                styles.button,
+                {
+                  backgroundColor:
+                    gameMode === 'BOT_EASY' ? '#4F5686' : '#191F24',
+                },
+              ]}>
+              Easy Bot
+            </Text>
+          </Pressable>
+
+          <Pressable onPress={() => setGameMode('BOT_MEDIUM')}>
+            <Text
+              style={[
+                styles.button,
+                {
+                  backgroundColor:
+                    gameMode === 'BOT_MEDIUM' ? '#4F5686' : '#191F24',
+                },
+              ]}>
+              Medium Bot
+            </Text>
+          </Pressable>
+        </View>
+      </ImageBackground>
+
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#242D34',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  bg: {
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    paddingTop: 15,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  map: {
+    width: '80%',
+    aspectRatio: 1,
   },
-  highlight: {
-    fontWeight: '700',
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  buttons: {
+    position: 'absolute',
+    bottom: 50,
+    flexDirection: 'row',
+  },
+  button: {
+    color: 'white',
+    margin: 10,
+    fontSize: 16,
+    backgroundColor: '#191F24',
+    padding: 10,
+    paddingHorizontal: 15,
   },
 });
-
-export default App;
